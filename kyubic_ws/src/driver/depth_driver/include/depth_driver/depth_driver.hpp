f0f3c7304578869575aacc0eaa400c3a0a4ddaff
kuwr-auv <kyutech.robosub@gmail.com>
2024-11-23 09:47:53 +0000

    feat: depth driver

---
+++ kyubic_ws/src/driver/depth_driver/include/depth_driver/depth_driver.hpp
@@ -0,0 +1,54 @@
+/**
+ * @file depth_driver.hpp
+ * @brief depth driver
+ * @author R.Ohnishi
+ * @date 2024/10/27
+ *
+ * @details 深度データを取得し，トピックに流す
+ *********************************************/
+
+#ifndef _DEPTH_DRIVER_HPP
+#define _DEPTH_DRIVER_HPP
+
+#include "depth_driver/bar30.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <driver_msgs/msg/depth.hpp>
+
+/**
+ * @namespace depth_driver
+ * @brief For depth driver
+ */
+namespace depth_driver
+{
+
+/**
+ * @brief Depth driver class
+ */
+class DepthDriver : public rclcpp::Node
+{
+public:
+  /**
+   * @brief Ba30 & Ros-topic setting
+   * @param portname Port name of serial device
+   * @param baudrate Baudrate of serial communication
+   * @details The ba30 serial setting and Define a callback function to flow the acquired data to
+   * the topic at 100ms interval
+   */
+  explicit DepthDriver(const char * _portname, const int _baudrate);
+
+private:
+  const char * portname;
+  const int baudrate;
+
+  std::shared_ptr<Bar30> bar30_;
+  rclcpp::Publisher<driver_msgs::msg::Depth>::SharedPtr pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  void update();
+};
+
+}  // namespace depth_driver
+
+#endif
---
+++ kyubic_ws/src/driver/depth_driver/src/bar30.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file bar30.cpp
+ * @brief bar30 library
+ * @author R.Ohnishi
+ * @date 2024/10/27
+ *
+ * @details bar30のデータを取得
+ **************************************************/
+
+#include "depth_driver/bar30.hpp"
+
+using namespace std::chrono_literals;
+
+namespace depth_driver
+{
+
+Bar30::Bar30(const char * _portname, const int _baudrate) : portname(_portname), baudrate(_baudrate)
+{
+  serial_ = std::make_shared<serial::Serial>(portname, baudrate);
+}
+
+bool Bar30::update()
+{
+  uint8_t buf[100] = {};
+  serial_->flush();
+  serial_->read_until(buf, sizeof(buf), 's', 10ms);
+  ssize_t len = serial_->read_until(buf, sizeof(buf), '\n', 10ms);
+  if (len > 0) {
+    // TODO: binaryで送れるようにする。s->fでエラーが起こると異常終了するので
+    std::string s_depth = std::string(buf, buf + len).substr(0, 6);
+    depth_data = std::stof(s_depth);
+
+    return true;
+  }
+  return false;
+}
+
+float Bar30::get_depth_data()
+{
+  return depth_data;
+}
+
+}  // namespace depth_driver
