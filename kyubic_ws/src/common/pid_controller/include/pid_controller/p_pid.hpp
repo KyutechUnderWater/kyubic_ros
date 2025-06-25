d3c400e566097c9b06d4dd5b831a11457fd6bcc6
kuwr-auv <kyutech.robosub@gmail.com>
2025-06-25 17:59:04 +0900

    feat: Velocity form P-PID

--- kyubic_ws/src/common/pid_controller/include/pid_controller/p_pid.hpp
+++ kyubic_ws/src/common/pid_controller/include/pid_controller/p_pid.hpp
@@ -17,7 +17,7 @@
 namespace pid_controller
 {
 
-class P_PID
+class PositionP_PID
 {
 private:
   double k, kp, ki, kd, kf;
@@ -30,12 +30,34 @@ private:
   std::shared_ptr<PositionPID> master_pid_, slave_pid_;
 
 public:
-  explicit P_PID(
+  explicit PositionP_PID(
     const double k, const double kp, const double ki, const double kd, const double kf,
     const double lo, const double hi);
 
   double update(
     double current_slave, double current_master, double target_master, double last_saturated);
+
+  void reset();
+};
+
+class VelocityP_PID
+{
+private:
+  double k, kp, ki, kd, kf;
+  double mlo, mhi, slo, shi;
+
+  double pre_p = 0;
+  std::chrono::high_resolution_clock::time_point pre_time =
+    std::chrono::high_resolution_clock::now();
+
+  std::shared_ptr<VelocityPID> master_pid_, slave_pid_;
+
+public:
+  explicit VelocityP_PID(
+    const double k, const double kp, const double ki, const double kd, const double kf,
+    const double mlo, const double mhi, const double slo, const double shi);
+
+  double update(double current_slave, double current_master, double target_master);
 
   void reset();
 };
--- kyubic_ws/src/common/pid_controller/src/p_pid.cpp
+++ kyubic_ws/src/common/pid_controller/src/p_pid.cpp
@@ -10,12 +10,13 @@
 #include "pid_controller/p_pid.hpp"
 
 #include <algorithm>
+#include <iostream>
 #include <memory>
 
 namespace pid_controller
 {
 
-P_PID::P_PID(
+PositionP_PID::PositionP_PID(
   const double k, const double kp, const double ki, const double kd, const double kf,
   const double lo, const double hi)
 : k(k), kp(kp), ki(ki), kd(kd), kf(kf), lo(lo), hi(hi)
@@ -24,7 +25,7 @@ P_PID::P_PID(
   slave_pid_ = std::make_shared<PositionPID>(kp, ki, kd, kf);
 }
 
-double P_PID::update(
+double PositionP_PID::update(
   double current_slave, double current_master, double target_master, double last_saturated)
 {
   double vel_ref = master_pid_->update(current_master, target_master, 0.0);
@@ -33,10 +34,26 @@ double P_PID::update(
   return slave_pid_->update(current_slave, vel_ref, last_saturated);
 }
 
-void P_PID::reset()
+void PositionP_PID::reset()
 {
   master_pid_->reset_integral();
   slave_pid_->reset_integral();
+}
+
+VelocityP_PID::VelocityP_PID(
+  const double k, const double kp, const double ki, const double kd, const double kf,
+  const double mlo, const double mhi, const double slo, const double shi)
+: k(k), kp(kp), ki(ki), kd(kd), kf(kf), mlo(mlo), mhi(mhi), slo(slo), shi(shi)
+{
+  master_pid_ = std::make_shared<VelocityPID>(k, 0.0, 0.0, 0.0, mlo, mhi);
+  slave_pid_ = std::make_shared<VelocityPID>(kp, ki, kd, kf, slo, shi);
+}
+
+double VelocityP_PID::update(double current_slave, double current_master, double target_master)
+{
+  double vel_ref = (target_master - current_master) * k;
+  vel_ref = std::clamp(vel_ref, mlo, mhi);
+  return slave_pid_->update(current_slave, vel_ref);
 }
 
 }  // namespace pid_controller
