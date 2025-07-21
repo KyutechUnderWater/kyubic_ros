4d93acdf9383243c8c99ab87a88ae6bfad8ff61c
atomon <38009289+atomon@users.noreply.github.com>
2025-07-21 10:18:51 +0900

    wip: check trajectory

--- kyubic_ws/src/tools/trajectory_viewer/setup.py
+++ kyubic_ws/src/tools/trajectory_viewer/setup.py
@@ -7,11 +7,9 @@ setup(
     version="0.0.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
-        ("share/ament_index/resource_index/packages",
-         ["resource/" + package_name]),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        ("share/" + package_name + "/launch",
-         ["launch/trajectory_viewer.launch.py"]),
+        ("share/" + package_name + "/launch", ["launch/trajectory_viewer.launch.py"]),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
@@ -21,6 +19,9 @@ setup(
     license="Apache License 2.0",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": [f"trajectory_viewer = {package_name}.main:main"],
+        "console_scripts": [
+            f"trajectory_viewer = {package_name}.main:main",
+            f"publish_odom_from_csv = {package_name}.publish_odom_from_csv:main",
+        ],
     },
 )
