cf025215f9a7cf8a5b8ee553153336666309a2e6
kuwr-auv <kyutech.robosub@gmail.com>
2025-10-01 22:05:14 +0900

    fix: memory error

--- kyubic_ws/src/localization/localization/src/localization_component.cpp
+++ kyubic_ws/src/localization/localization/src/localization_component.cpp
@@ -40,6 +40,7 @@ Localization::Localization(const rclcpp::NodeOptions & options) : Node("localiza
 
   odom_msg_ = std::make_shared<localization_msgs::msg::Odometry>();
   gnss_msg_ = std::make_shared<driver_msgs::msg::Gnss>();
+  imu_raw_msg_ = std::make_shared<driver_msgs::msg::IMU>();
   global_pose_msg_ = std::make_shared<localization_msgs::msg::GlobalPose>();
   global_pose_msg_->coordinate_system_id = coord_system_id;
 
@@ -131,12 +132,12 @@ void Localization::gnss_callback(driver_msgs::msg::Gnss::UniquePtr msg)
 void Localization::imu_raw_callback(driver_msgs::msg::IMU::UniquePtr msg)
 {
   imu_raw_msg_ = std::move(msg);
-  RCLCPP_DEBUG(this->get_logger(), "Updated GNSS data");
+  RCLCPP_DEBUG(this->get_logger(), "Updated IMU data");
 }
 
 void Localization::_calc_global_pose(const localization_msgs::msg::Odometry::SharedPtr odom_)
 {
-  double azimuth_rad = (azimuth - reference_plane.meridian_convergence) * std::numbers::pi / 180;
+  double azimuth_rad = (azimuth + reference_plane.meridian_convergence) * std::numbers::pi / 180;
   double plane_x =
     odom_->pose.position.x * cos(azimuth_rad) - odom_->pose.position.y * sin(azimuth_rad);
   double plane_y =
