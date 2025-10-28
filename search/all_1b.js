2c4b0f3d3641b19236e104d3f5f6117f57af5656
atomon <38009289+atomon@users.noreply.github.com>
2025-10-28 06:15:29 +0000

    Deploying to gh-pages from @ KyutechUnderWater/kyubic_ros@e1463965d9d816e4d27d813eaf3b68906e77078d 🚀

--- search/all_1b.js
+++ search/all_1b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['_7edvldriver_0',['~DVLDriver',['../classdvl__driver_1_1DVLDriver.html#ad9e5b3dd4dc8bb73f6a026e8b22100e3',1,'dvl_driver::DVLDriver']]],
-  ['_7eserial_1',['~Serial',['../classserial_1_1Serial.html#a071000a2f5f77a40df8311fad5044481',1,'serial::Serial']]]
+  ['_7eg366_1',['~G366',['../classimu__driver_1_1g366_1_1G366.html#a95e232c48f9b99fbb9f96b0415780051',1,'imu_driver::g366::G366']]],
+  ['_7eserial_2',['~Serial',['../classserial_1_1Serial.html#a071000a2f5f77a40df8311fad5044481',1,'serial::Serial']]]
 ];
