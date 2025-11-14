31ff902ec1f7b4dd8ff9e7cb28d1e150adff4f53
KingKouara <93708295+Kingkouara@users.noreply.github.com>
2025-11-14 09:45:17 +0000

    feat: pdla action

--- kyubic_ws/src/control/automatic/planning/projection_dynamic_look_ahead_planner/include/projection_dynamic_look_ahead_planner/pdla_planner.hpp
+++ kyubic_ws/src/control/automatic/planning/projection_dynamic_look_ahead_planner/include/projection_dynamic_look_ahead_planner/pdla_planner.hpp
@@ -1,21 +1,23 @@
 /**
- * @file PDLA_planner.hpp
- * @brief Tracking through the pass with Projection Dynamic Look-Ahead Planner
+ * @file pdla_planner.hpp
+ * @brief Tracking through the pass with Projection Dynamic Look-Ahead Planner (Action Server)
  * @author R.Ohnishi
- * @date 2025/05/13
+ * @date 2025/11/06 (Modified)
  *
- * @details ベクトル射影を用いた動的前方注視制御による経路追従
- ****************************************************************************/
+ * @details ベクトル射影を用いた動的前方注視制御による経路追従 (Action Server)
+ ********************************************************************************************/
 
-#ifndef _PLDA_PLANNER_HPP
-#define _PLDA_PLANNER_HPP
-
-#include <sys/types.h>
+#ifndef _PDLA_PLANNER_HPP
+#define _PDLA_PLANNER_HPP
 
 #include <localization_msgs/msg/odometry.hpp>
+#include <mutex>
 #include <path_planner/path_csv_loader.hpp>
+#include <planner_msgs/action/pdla.hpp>
 #include <planner_msgs/msg/wrench_plan.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <rclcpp_action/server_goal_handle.hpp>
 
 namespace planner
 {
@@ -31,28 +33,44 @@ struct Tolerance
 
 class PDLAPlanner : public rclcpp::Node
 {
+public:
+  explicit PDLAPlanner(const rclcpp::NodeOptions & options);
+
 private:
-  rclcpp::Publisher<planner_msgs::msg::WrenchPlan>::SharedPtr pub_;
-  rclcpp::Subscription<localization_msgs::msg::Odometry>::SharedPtr sub_;
-
-  std::vector<PoseData> target_pose_;
-  std::shared_ptr<localization_msgs::msg::Odometry> current_odom_;
-
   double look_ahead_scale;
   Tolerance reach_tolerance, waypoint_tolerance;
 
-  bool catmull_rom = false;
-  uint step_idx = 0;
+  rclcpp::CallbackGroup::SharedPtr callback_group_;
+
+  rclcpp::Publisher<planner_msgs::msg::WrenchPlan>::SharedPtr pub_;
+  rclcpp::Subscription<localization_msgs::msg::Odometry>::SharedPtr sub_;
+  rclcpp_action::Server<planner_msgs::action::PDLA>::SharedPtr action_server_;
+
+  // Action Callbacks
+  rclcpp_action::GoalResponse handle_goal(
+    const rclcpp_action::GoalUUID & uuid,
+    std::shared_ptr<const planner_msgs::action::PDLA::Goal> goal);
+  rclcpp_action::CancelResponse handle_cancel(
+    const std::shared_ptr<rclcpp_action::ServerGoalHandle<planner_msgs::action::PDLA>> goal_handle);
+  void handle_accepted(
+    const std::shared_ptr<rclcpp_action::ServerGoalHandle<planner_msgs::action::PDLA>> goal_handle);
 
   void odometryCallback(const localization_msgs::msg::Odometry::SharedPtr msg);
 
-  void _updateGoal();
+  void _runPlannerLogic(
+    const std::shared_ptr<rclcpp_action::ServerGoalHandle<planner_msgs::action::PDLA>> &
+      goal_handle);
   bool _checkReached(PoseData & target_pose, Tolerance tolerance);
-
   void _print_waypoint(std::string label, size_t step_idx);
 
-public:
-  explicit PDLAPlanner(const rclcpp::NodeOptions & options);
+  std::vector<PoseData> target_pose_;
+
+  std::mutex odom_mutex_;
+  std::mutex goal_mutex_;
+  std::shared_ptr<localization_msgs::msg::Odometry> current_odom_;
+  std::shared_ptr<rclcpp_action::ServerGoalHandle<planner_msgs::action::PDLA>> active_goal_handle_;
+
+  size_t step_idx = 0;
 };
 
 }  // namespace planner
