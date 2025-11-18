29e5846033b81edaea2988364d688130cff07128
atomon <38009289+atomon@users.noreply.github.com>
2025-11-18 05:18:59 +0000

    feat: change to lifecycle node

--- kyubic_ws/src/control/manual/joy2wrench/include/joy2wrench/joy2wrench.hpp
+++ kyubic_ws/src/control/manual/joy2wrench/include/joy2wrench/joy2wrench.hpp
@@ -1,25 +1,54 @@
-#include <geometry_msgs/msg/wrench_stamped.hpp>
-#include <joy_common_msgs/msg/joy.hpp>
-#include <rclcpp/rclcpp.hpp>
-#include <string>
+/**
+ * @file joy2wrench.hpp
+ * @brief manual control library
+ * @author R.Ohnishi
+ * @date 2024/11/27
+ *
+ * @details joy stickの情報をロボットの操作量に変換
+ **************************************************/
+
+#ifndef _JOY2WRENCH_HPP
+#define _JOY2WRENCH_HPP
+
+#include "geometry_msgs/msg/wrench_stamped.hpp"
+#include "joy_common_msgs/msg/joy.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "rclcpp_lifecycle/lifecycle_publisher.hpp"
 
 namespace joy2wrench
 {
 
-class Joy2WrenchStamped : public rclcpp::Node
+class Joy2WrenchStamped : public rclcpp_lifecycle::LifecycleNode
 {
 public:
   explicit Joy2WrenchStamped(const rclcpp::NodeOptions & options);
 
+  // Lifecycle Callback
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_configure(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_activate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_deactivate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_cleanup(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_shutdown(
+    const rclcpp_lifecycle::State & previous_state) override;
+
 private:
-  std::string device_name;
   double force_x_scale, force_y_scale, force_z_scale;
   double torque_x_scale, torque_z_scale;
 
-  rclcpp::Publisher<geometry_msgs::msg::WrenchStamped>::SharedPtr pub_;
+  rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::WrenchStamped>::SharedPtr pub_;
   rclcpp::Subscription<joy_common_msgs::msg::Joy>::SharedPtr sub_;
 
   void _joyCallback(const joy_common_msgs::msg::Joy::SharedPtr msg);
 };
 
 }  // namespace joy2wrench
+
+#endif
--- kyubic_ws/src/control/manual/joy2wrench/src/joy2wrench.cpp
+++ kyubic_ws/src/control/manual/joy2wrench/src/joy2wrench.cpp
@@ -1,11 +1,29 @@
+/**
+ * @file joy2wrench.cpp
+ * @brief manual control library
+ * @author R.Ohnishi
+ * @date 2024/11/27
+ *
+ * @details joy stickの情報をロボットの操作量に変換
+ **************************************************/
+
 #include "joy2wrench/joy2wrench.hpp"
 
 using namespace std::chrono_literals;
 
 namespace joy2wrench
 {
+
 Joy2WrenchStamped::Joy2WrenchStamped(const rclcpp::NodeOptions & options)
-: Node("joy_to_wrench_stamped", options)
+: rclcpp_lifecycle::LifecycleNode("joy_to_wrench_stamped_lifecycle", options)
+{
+}
+
+/**
+ * @brief "configuring" State Callback
+ */
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+Joy2WrenchStamped::on_configure(const rclcpp_lifecycle::State &)
 {
   force_x_scale = this->declare_parameter("force_x_scale", 1.0);
   force_y_scale = this->declare_parameter("force_y_scale", 1.0);
@@ -17,10 +35,61 @@ Joy2WrenchStamped::Joy2WrenchStamped(const rclcpp::NodeOptions & options)
   pub_ = create_publisher<geometry_msgs::msg::WrenchStamped>("robot_force", qos);
   sub_ = create_subscription<joy_common_msgs::msg::Joy>(
     "joy_common", 10, std::bind(&Joy2WrenchStamped::_joyCallback, this, std::placeholders::_1));
+
+  RCLCPP_INFO(get_logger(), "Configuration successful. Transitioning to Inactive.");
+  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+}
+
+/**
+ * @brief "activating" State Callback
+ */
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+Joy2WrenchStamped::on_activate(const rclcpp_lifecycle::State & state)
+{
+  LifecycleNode::on_activate(state);
+  RCLCPP_INFO(get_logger(), "Activation successful. Transitioning to Active.");
+  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+}
+
+/**
+ * @brief "deactivating" State Callback
+ */
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+Joy2WrenchStamped::on_deactivate(const rclcpp_lifecycle::State & state)
+{
+  RCLCPP_INFO(get_logger(), "Deactivation successful. Transitioning to Inactive.");
+  LifecycleNode::on_deactivate(state);
+  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+}
+
+/**
+ * @brief "cleaningup" State Callback
+ */
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+Joy2WrenchStamped::on_cleanup(const rclcpp_lifecycle::State &)
+{
+  pub_.reset();
+  sub_.reset();
+  RCLCPP_INFO(get_logger(), "Cleanup successful. Transitioning to Unconfigured.");
+  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+}
+
+/**
+ * @brief "shuttingdown" State Callback
+ */
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+Joy2WrenchStamped::on_shutdown(const rclcpp_lifecycle::State &)
+{
+  pub_.reset();
+  sub_.reset();
+  RCLCPP_INFO(get_logger(), "Shutdown successful.");
+  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
 void Joy2WrenchStamped::_joyCallback(const joy_common_msgs::msg::Joy::SharedPtr msg)
 {
+  if (!pub_) return;
+
   auto wrench_msg = std::make_unique<geometry_msgs::msg::WrenchStamped>();
   wrench_msg->header = msg->header;
 
@@ -41,6 +110,7 @@ void Joy2WrenchStamped::_joyCallback(const joy_common_msgs::msg::Joy::SharedPtr 
 
   pub_->publish(std::move(wrench_msg));
 }
+
 }  // namespace joy2wrench
 
 #include "rclcpp_components/register_node_macro.hpp"
