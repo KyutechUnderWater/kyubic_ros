955b53d4382f1ff18f6f9d7a7abb2a1cb0355654
atomon <38009289+atomon@users.noreply.github.com>
2025-11-18 14:32:16 +0000

    fix: multiple definition

--- kyubic_ws/src/control/manual/joy_common/include/joy_common/joy_common.hpp
+++ kyubic_ws/src/control/manual/joy_common/include/joy_common/joy_common.hpp
@@ -7,6 +7,9 @@
  * @details joystickのデータを流す
  *********************************/
 
+#ifndef _JOY_COMMON_HPP
+#define _JOY_COMMON_HPP
+
 #include <joy_common_msgs/msg/buttons.hpp>
 #include <joy_common_msgs/msg/joy.hpp>
 #include <rclcpp/rclcpp.hpp>
@@ -20,36 +23,7 @@ using ButtonValue = std::variant<bool, double>;
 using ButtonMap =
   std::map<std::string, std::function<ButtonValue(const joy_common_msgs::msg::Buttons &)>>;
 
-ButtonMap get_button_map()
-{
-  std::map<std::string, std::function<ButtonValue(const joy_common_msgs::msg::Buttons &)>>
-    button_getters;
-
-  // --- Bool 型のフィールド ---
-  button_getters["circle"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.circle; };
-  button_getters["cross"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.cross; };
-  button_getters["triangle"] = [](const joy_common_msgs::msg::Buttons & msg) {
-    return msg.triangle;
-  };
-  button_getters["square"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.square; };
-  button_getters["up"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.up; };
-  button_getters["down"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.down; };
-  button_getters["left"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.left; };
-  button_getters["right"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.right; };
-  button_getters["l_stick"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.l_stick; };
-  button_getters["r_stick"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.r_stick; };
-  button_getters["l1"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.l1; };
-  button_getters["r1"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.r1; };
-  button_getters["select"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.select; };
-  button_getters["start"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.start; };
-  button_getters["ps"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.ps; };
-
-  // --- Float64 型のフィールド ---
-  button_getters["l2"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.l2; };
-  button_getters["r2"] = [](const joy_common_msgs::msg::Buttons & msg) { return msg.r2; };
-
-  return button_getters;
-}
+ButtonMap get_button_map();
 
 class JoyCommon : public rclcpp::Node
 {
@@ -68,3 +42,5 @@ private:
 };
 
 }  // namespace joy_common
+
+#endif  // !_JOY_COMMON_HPP
