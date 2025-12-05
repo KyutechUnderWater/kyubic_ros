838dbbfe6c0876c237a84d965a93c76c052f751a
kuwr-auv <33391219+kuwr-auv@users.noreply.github.com>
2025-12-05 05:21:11 +0000

    feat: add system status

--- kyubic_ws/src/driver/sensors_esp32_driver/launch/sensors_esp32_driver.launch.py
+++ kyubic_ws/src/driver/sensors_esp32_driver/launch/sensors_esp32_driver.launch.py
@@ -2,22 +2,8 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
-from launch_ros.actions import ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-
-
-def snake_to_pascal(text):
-    """
-    snake_case -> PascalCase
-    (ex: button_battery_state_component -> ButtonBatteryState)
-    """
-    if text == "rgb_component":
-        return "RGB"
-
-    if text.endswith("_component"):
-        text = text.replace("_component", "")
-    return "".join(x.title() for x in text.split("_"))
 
 
 def generate_launch_description():
@@ -31,55 +17,47 @@ def generate_launch_description():
 
     log_level_arg = DeclareLaunchArgument(
         "log_level",
-        default_value=["info"],
+        default_value="info",
         description="Logging level",
     )
 
     executable_list = [
-        "button_battery_state_component",
-        "buzzer_switch_component",
-        "depth_component",
-        "environment_component",
-        "imu_reset_component",
-        "leak_component",
-        "rgb_component",
-        "rtc_gnss_component",
-        "rtc_time_component",
-        "tilt_servo_component",
-        "zed_power_component",
+        "button_battery_state_component_node",
+        "buzzer_switch_component_node",
+        "depth_component_node",
+        "environment_component_node",
+        "imu_reset_component_node",
+        "leak_component_node",
+        "rgb_component_node",
+        "rtc_gnss_component_node",
+        "rtc_time_component_node",
+        "system_status_component_node",
+        "tilt_servo_component_node",
+        "zed_power_component_node",
     ]
 
-    composable_nodes = []
+    nodes = []
     for exe in executable_list:
         node_remappings = []
-        if exe == "depth_component":
+        if exe == "depth_component_node":
             node_remappings.append(("depth", "/driver/depth"))
 
-        composable_nodes.append(
-            ComposableNode(
-                package="sensors_esp32_driver",
-                plugin=f"sensors_esp32_driver::{snake_to_pascal(exe)}",
-                namespace="sensors_esp32_driver",
-                name=exe,
-                parameters=[config],
-                remappings=node_remappings,
-                extra_arguments=[{"use_intra_process_comms": True}],
-            )
+        # Nodeの定義
+        node = Node(
+            package="sensors_esp32_driver",
+            executable=exe,
+            namespace="sensors_esp32_driver",
+            name=exe,
+            parameters=[config],
+            remappings=node_remappings,
+            arguments=["--ros-args", "--log-level", LaunchConfiguration("log_level")],
+            output="screen",
         )
-
-    container = ComposableNodeContainer(
-        name="sensors_esp32_driver_container",
-        namespace="sensors_esp32_driver",
-        package="rclcpp_components",
-        executable="component_container_mt",
-        composable_node_descriptions=composable_nodes,
-        arguments=["--ros-args", "--log-level", LaunchConfiguration("log_level")],
-        output="screen",
-    )
+        nodes.append(node)
 
     return LaunchDescription(
         [
             log_level_arg,
-            container,
         ]
+        + nodes
     )
